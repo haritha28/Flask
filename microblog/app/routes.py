da09ce641b8ab80a0cbbe4e171b2e1025e77bccb
Haritha28 <haritha.h20@gmail.com>
2019-02-07 09:52:09 +0100

    Added an HTML page as return for the function

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -4,5 +4,19 @@ from app import app
 #@app.route are decorators. We can use decorators to register them to function callbacks to a certain event
 @app.route('/')
 @app.route('/index')
+
+#Initially the view function returned a string
+#Now we expand it to return a complete HTML page
+
 def index():
-    return "Hello, World!"
+    user = {'username' : 'Haritha'}
+    return '''
+<html>
+    <head>
+        <title> Home Page - Microblog </title>
+    </head>
+    <body>
+        <h1>Hello, ''' + user['username'] + '''!</h1>
+    </body>
+</html>'''
+
