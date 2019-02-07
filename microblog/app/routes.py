48d3b89d258860571b63d23b88cb3518b932e307
Haritha28 <haritha.h20@gmail.com>
2019-02-07 10:12:10 +0100

    Added templates via rendering method

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,3 +1,4 @@
+from flask import render_template
 from app import app
 
 # route() is used to bind a URL to a function.
@@ -8,15 +9,21 @@ from app import app
 #Initially the view function returned a string
 #Now we expand it to return a complete HTML page
 
+#The function render template is used to import a function that comes with the Flask framework.
+#Fucntion has template name and the lsit of arguments used by the HTML page.
 def index():
     user = {'username' : 'Haritha'}
-    return '''
-<html>
-    <head>
-        <title> Home Page - Microblog </title>
-    </head>
-    <body>
-        <h1>Hello, ''' + user['username'] + '''!</h1>
-    </body>
-</html>'''
+    return render_template('index.html', title='Home', user=user)
 
+
+#<html>
+#    <head>
+#        <title> Home Page - Microblog </title>
+#    </head>
+#    <body>
+#        <h1>Hello, ''' + user['username'] + '''!</h1>
+#   </body>
+#</html>
+
+##Adding the HTML file in the return would give us a complex code and hence we seperate the layout and logic of the
+#application seperately. For this we use templates
